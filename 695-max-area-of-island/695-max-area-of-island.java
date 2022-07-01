48aab04f0103cab2cadacb615875c4dfac5be2db
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2022-07-01 01:07:19 -0400

    Time: 3 ms (83.21%), Space: 47.5 MB (42.84%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -0,0 +1,32 @@
+class Solution {
+    int count = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+       int max = Integer.MIN_VALUE;
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
+                if(grid[i][j] == 1){
+                    count=0;
+                    dfs(grid,rows,cols,i,j);
+                    max = Math.max(max,count);
+                }
+            }
+        }
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
+    
+    public void dfs(int[][] grid,int rows,int cols,int i,int j){
+        if(i < 0 || i>=rows || j < 0 || j >=cols || grid[i][j] != 1){
+            return;
+        }
+        
+        grid[i][j] = 2;
+        count++;
+        dfs(grid,rows,cols,i-1,j);
+        dfs(grid,rows,cols,i+1,j);
+        dfs(grid,rows,cols,i,j+1);
+        dfs(grid,rows,cols,i,j-1); 
+    }
+    
+}
