edd60b18bd58f0a66c2cbbfeb8b0aa0752f9a419
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2022-07-04 20:19:15 -0400

    Time: 15 ms (91.00%), Space: 13.7 MB (11.60%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        #List[str]) -> List[str]:
+        # quick check on the characters,
+        #   otherwise it would exceed the time limit for certain test cases.
+        #if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
+           # return []
+
+        wordSet = set(wordDict)
+
+        dp = [[]] * (len(s)+1)
+        dp[0] = [""]
+
+        for endIndex in range(1, len(s)+1):
+            sublist = []
+            # fill up the values in the dp array.
+            for startIndex in range(0, endIndex):
+                word = s[startIndex:endIndex]
+                if word in wordSet:
+                    for subsentence in dp[startIndex]:
+                        sublist.append((subsentence + ' ' + word).strip())
+
+            dp[endIndex] = sublist
+
+        return dp[len(s)]
