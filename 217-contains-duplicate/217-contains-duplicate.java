51e6b746362baaa666d316eca3fe2f4a50b98727
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2022-07-27 20:04:04 -0400

    Time: 6 ms (96.83%), Space: 68.8 MB (61.25%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int n=nums.length;
+       //Solution 1
+        /* int n=nums.length;
         boolean b=false;
         Arrays.sort(nums);
         for(int i=0;i<n-1;i++){
@@ -8,6 +9,15 @@ class Solution {
                 b=true;
             }
         }
-        return b;
+        return b; */
+       //Solution 2
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0; i<nums.length;i++){
+            if(!set.add(nums[i])){
+                return true;                
+            }
+        }
+       
+        return false;
     }
 }
