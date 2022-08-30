b16b83c110b72020ef4e8098bd7675f6d2a2e8f3
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2022-08-30 14:59:42 -0400

    Time: 0 ms (100.00%), Space: 42.8 MB (57.35%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        for(int i =0;i<rows;i++){
+           for(int j =i;j<cols;j++){
+             int temp = matrix[i][j];
+               matrix[i][j] = matrix[j][i];
+               matrix[j][i] = temp;
+           } 
+        } 
+            
+        for(int i =0;i<rows;i++){
+           for(int j =0;j<rows/2;j++){
+             int temp = matrix[i][j];
+               matrix[i][j] = matrix[i][matrix.length -j -1];
+               matrix[i][matrix.length -j -1] = temp;
+           } 
+        } 
+        }
+        
+    }
+    
