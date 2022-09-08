d5db5b63d95251007fd3faa285c0590d0a18fa78
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2022-09-08 02:42:55 -0400

    Time: 9 ms (34.25%), Space: 46.2 MB (73.57%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,35 @@
+class Solution {
+    int [] parent;
+    public int countComponents(int n, int[][] edges) {
+        int cnt = n;
+        parent = new int[n];
+        for(int i=0;i<parent.length;i++){
+            parent[i] = i;
+        }
+        
+        for(int[] edge : edges){
+            if(find(edge[0]) != find(edge[1])){
+                
+            union(edge[0],edge[1]);
+            cnt--;
+            }
+        }
+        
+        return cnt;
+    }
+    
+    public int find(int node){
+        while(parent[node] != node){
+            node = parent[node];
+        }
+        return node;
+    }
+    
+    public void union(int node_i , int node_j){
+        int rootNodei = find(node_i);
+        int rootNodej = find(node_j);
+        if(rootNodei != rootNodej ){
+            parent[rootNodei] = rootNodej;
+        }
+    }
+}
