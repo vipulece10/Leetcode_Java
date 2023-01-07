382f44d166dc1e718743b56aa94792fe441033a9
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-01-07 18:47:09 -0500

    Time: 6 ms (45.92%), Space: 62.8 MB (64.55%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        
+        int len = gas.length;
+        if(len==1) return gas[0]>=cost[0] ? 0 : -1;
+		
+        int start=0,end=1;
+        int currFuel=gas[start]-cost[start];
+        while(start<len)
+        {
+            while(currFuel>=0)
+            {
+                currFuel+=gas[end]-cost[end];
+                end=(end+1)%len;
+                // completed the circle
+                if (end == start && currFuel >= 0) return start;
+            }
+            while(currFuel < 0 && start < len)
+            {
+                currFuel-=gas[start]-cost[start];
+                start++;
+            }
+        }
+        return -1;
+        
+    }
+}
