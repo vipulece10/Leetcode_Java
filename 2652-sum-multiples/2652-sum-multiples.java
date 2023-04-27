84030619554425265900c2e75078363ae518593d
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-04-27 18:20:15 -0400

    Time: 4 ms (94.45%), Space: 40.1 MB (35.52%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int i = 1;
+        int sum = 0;
+        while(i <= n){
+            if((i % 3 == 0) || (i % 5 == 0) ||(i % 7 == 0)){
+                sum += i;
+            }
+            i++;
+        }
+        return sum;
+    }
+}
