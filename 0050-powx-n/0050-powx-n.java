a1f74bf30b5a11c96b6c8769d4cd2ea7eece0ada
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-07-24 11:07:16 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (84.40%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,26 @@
+class Solution {
+    //double ans;
+    public double myPow(double x, int n) {
+        if(n==0)
+            return 1;
+            
+        if(n<0){
+           // n=-n;
+            x=1/x;
+            if(n%2==0){
+            return myPow(x*x,-(n/2));
+            }
+            else{
+            return x*myPow(x,-(n+1));
+            }
+        }
+        if(n%2==0){
+          return myPow(x*x,n/2);
+        }
+        else{
+            return x*myPow(x,n-1);
+        }
+       // return ans;
+        
+    }
+}
