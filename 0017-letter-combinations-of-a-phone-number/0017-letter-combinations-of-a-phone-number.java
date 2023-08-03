f23168bc429c9c44a6736b56d14812598e4a9a45
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-08-03 17:56:04 -0400

    Time: 11 ms (11.91%), Space: 41.7 MB (7.12%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        ArrayList<String> res=new ArrayList<>();
+        if(digits.length()==0) return res;
+        Map<Character,String> map=new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+       // ArrayList<String> res=new ArrayList<>();
+      //  StringBuilder sb=new StringBuilder();
+        dfs(digits,"",map,res,0);
+        return res;
+    }
+    
+    public void dfs(String digits,String sb,Map<Character,String>map,List<String>res,int index){
+        System.out.print("  "+digits);
+        
+    if(index==digits.length()){
+        res.add(sb);
+        return;
+    }
+    String letter=map.get(digits.charAt(index));
+    System.out.print(letter);
+    for(int i=0;i<letter.length();i++) {
+        dfs(digits,sb+letter.charAt(i),map,res,index+1);
+      //  sb.re(sb.length()-1);
+    }
+}
+    
+}
