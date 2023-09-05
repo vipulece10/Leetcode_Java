145eb55d369f4b760e15e32ddf8beba298bae476
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-09-05 10:49:00 -0400

    Time: 33 ms (39.25%), Space: 53.1 MB (71.35%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        /*
+        slots1 = [[10,50],[60,120],[140,210]], 
+        slots2 = [[0,15],[60,70]]
+        */
+        List<Integer> res = new ArrayList<>();
+        Arrays.sort(slots1,(a,b)->a[0] - b[0]);
+        Arrays.sort(slots2,(a,b)->a[0] - b[0]);
+        
+        int i = 0; //iterate on slots1
+        int j = 0; //iterate on slots2
+        while(i < slots1.length && j < slots2.length){
+            int start = Math.max(slots1[i][0],slots2[j][0]);
+            int end = Math.min(slots1[i][1],slots2[j][1]);
+            if(start + duration <= end){
+                res.add(start);
+                res.add(start + duration);
+                return res;
+            }
+            if(slots1[i][1] < slots2[j][1]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return res;
+    }
+}
