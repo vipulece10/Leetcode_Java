6ba20c7d8b1acae148590f24409413bdd6c26c13
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-09-14 23:09:15 -0400

    Time: 29 ms (65.87%), Space: 44 MB (84.95%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int partitionString(String s) {
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        int i =0;
+        /*
+        //    abacaba   set [ac]     count = 2     ab,ac
+        
+        */
+        while(i<s.length()){
+           if (!set.add(s.charAt(i))){
+               count++;
+               set.clear();   
+           }else{
+               i++;
+           }
+        }
+        
+        return count+1;
+    }
+}
+
+//    abacaba
+// {a:1 b: 1}  count++ {ab}
+//{a:3,b:1,c:1} count++ {ac}
