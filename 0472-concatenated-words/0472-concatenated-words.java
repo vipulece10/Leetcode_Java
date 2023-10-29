282484557ef0fc12fd6ebe0b8cf3f4058d0fceb2
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-10-29 14:19:51 -0400

    Time: 51 ms (74.94%), Space: 48.3 MB (63.11%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,32 +1,25 @@
 class Solution {
-     private boolean dfs(final String word, int length, final boolean[] visited, final Set<String> dictionary) {
-        if (length == word.length()) {
-            return true;
-        }
-        if (visited[length]) {
-            return false;
-        }
-        visited[length] = true;
-        for (int i = word.length() - (length == 0 ? 1 : 0); i > length; --i) {
-            if (dictionary.contains(word.substring(length, i)) 
-                && dfs(word, i, visited, dictionary)) {
-                return true;
-            }
-            
-        }
-        return false;
+    //https://leetcode.com/problems/concatenated-words/discuss/541520/Java-DFS-%2B-Memoization-Clean-code
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+          List<String> ans = new ArrayList<>();
+        HashSet<String> wordSet = new HashSet<>(Arrays.asList(words));
+        HashMap<String, Boolean> cache = new HashMap<>();
+        for (String word : words) if (dfs(word, wordSet, cache)) ans.add(word);
+        return ans;
     }
     
-    public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        final Set<String> dictionary = new HashSet<>(Arrays.asList(words));
-        final List<String> answer = new ArrayList<>();
-        for (final String word : words) {
-            final int length = word.length();
-            final boolean[] visited = new boolean[length];
-            if (dfs(word, 0, visited, dictionary)) {
-                answer.add(word);
+    boolean dfs(String word, HashSet<String> wordSet, HashMap<String, Boolean> cache) {
+        if (cache.containsKey(word)) return cache.get(word);
+        for (int i = 1; i < word.length(); i++) {
+            if (wordSet.contains(word.substring(0, i))) {
+                String suffix = word.substring(i);
+                if (wordSet.contains(suffix) || dfs(suffix, wordSet, cache)) {
+                    cache.put(word, true);
+                    return true;
+                }
             }
         }
-        return answer;   
+        cache.put(word, false);
+        return false;
     }
 }
