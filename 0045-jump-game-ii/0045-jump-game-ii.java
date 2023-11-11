0d27b584c57b608105dee08d428956236ff5d612
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-11-11 16:31:06 -0500

    Time: 1 ms (99.28%), Space: 45 MB (8.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -4,9 +4,9 @@ class Solution {
         int start = 0;
         int end =0;
         int jump=0;
-        //int farthest = 0;
+        int farthest = 0;
         while(end < nums.length - 1){
-            int farthest = 0;
+            //int farthest = 0;
             for(int i=start;i<=end;i++){
                 farthest = Math.max(farthest,i+nums[i]);
             }
