700d517a6a88c0084c1478a1fa333a46106588a8
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2023-11-28 21:04:12 -0500

    Time: 18 ms (12.52%), Space: 43.1 MB (43.50%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        int result = Integer.MIN_VALUE;
+        for(int i =0 ; i < nums.length - 2;i++){
+           int low = i+1;
+           int high = nums.length - 1;
+           while(low < high){
+               int curr_sum = nums[i] + nums[low] + nums[high];
+               
+               if(curr_sum < target)
+                   low++;
+               else 
+                   high--;
+                //System.out.println(diff); 
+              if(Math.abs(target -  curr_sum) < Math.abs(diff)){
+                  diff = target - curr_sum;
+                  result = curr_sum;
+              }
+                  
+           }
+          // System.out.println(diff); 
+        }
+        System.out.println(diff);
+        //return target - diff;
+        return result;
+    }
+}
