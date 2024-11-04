d401858cd6529dcb9632cd5b1bc38632147ec7a8
Vipul Tiwari <40255509+vipulece10@users.noreply.github.com>
2024-11-03 19:34:06 -0800

    Time: 43 ms (16.95%), Space: 46.6 MB (32.92%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -1,51 +1,20 @@
 class Solution {
-//     public String compressedString(String word) {
-//         StringBuilder sb = new StringBuilder();
-//         int n = word.length();
-//         for(int i = 0;i<word.length();i++){
-//             int count =1;
-//             while(i < n-1 && word.charAt(i) == word.charAt(i+1)){
-//                 count++;
-//                 i++;
-//                 if(count == 9){
-//                    // sb.append(String.valueOf(count) + word.charAt(i));
-//                     break;
-//                 }
-//             }
-//             sb.append(String.valueOf(count) + word.charAt(i));
-            
-//         }
-//         return sb.toString();
-//     }
-    
-    public String compressedString(String word){
-        /*
-        "aaaaaaaaaaaaaabb"
-        
-        */
-        
+    public String compressedString(String word) {
         StringBuilder sb = new StringBuilder();
-        int count = 1;
-        
-        for(int i = 0;i<word.length() - 1; i++){
-            if(word.charAt(i) == word.charAt(i+1)){
+        int n = word.length();
+        for(int i = 0;i<word.length();i++){
+            int count =1;
+            while(i < n-1 && word.charAt(i) == word.charAt(i+1)){
+                count++;
+                i++;
                 if(count == 9){
-                sb.append(count);
-                sb.append(word.charAt(i));
-                count = 1;
-                   count = 1; 
-                }else{
-                   count++; 
+                   // sb.append(String.valueOf(count) + word.charAt(i));
+                    break;
                 }
-            }else{
-                sb.append(count);
-                sb.append(word.charAt(i));
-                count = 1;
             }
+            sb.append(String.valueOf(count) + word.charAt(i));
+            
         }
-        
-        sb.append(count);
-        sb.append(word.charAt(word.length() - 1));
         return sb.toString();
     }
 }
